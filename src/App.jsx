f014d008e4ed3e75c9c9ccca44d32361c96a4f5b
jmoreno-dev <ytanonim12@gmail.com>
2025-11-22 22:08:09 +0100

    added all components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import Welcome from './components/Welcome/Welcome.jsx'
 import Login from './components/Login/Login.jsx'
+import Register from './components/Register/Register.jsx'
+import ForgotPassword from './components/ForgotPassword/ForgotPassword.jsx'
+import Home from './components/Home/Home.jsx'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
     <Routes>
       <Route path='/' element={<Welcome />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
   )
 }
