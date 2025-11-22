5d15555cbbde029293b643174bff5998890eabf0
jmoreno-dev <ytanonim12@gmail.com>
2025-11-22 22:07:05 +0100

    added register module

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,11 @@
+import styles from "./Register.module.css";
+function Register() {
+    return (
+        <section className={styles["register-container"]}>
+            <h1>Register Page</h1>
+            {/* Registration form elements go here */}
+        </section>
+    );
+}
+
+export default Register;
