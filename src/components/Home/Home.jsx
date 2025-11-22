10e136854a2a138f9f17a3276b6ff71a1ca5c577
jmoreno-dev <ytanonim12@gmail.com>
2025-11-22 22:07:39 +0100

    added home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <section>
+            <h1>Home Page</h1>
+            <p>Welcome to the Home Page of the School Manager App!</p>
+        </section>
+    );
+}
+export default Home;
