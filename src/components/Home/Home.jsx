fb8e9cdd51ab41fafa8126bb0d4cd05108e4dc8f
jmoreno-dev <ytanonim12@gmail.com>
2025-11-28 22:19:41 +0100

    added header and home container styles for improved layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
+import styles from "./Home.module.css";
+
 function Home() {
     return (
-        <section>
-            <h1>Home Page</h1>
-            <p>Welcome to the Home Page of the School Manager App!</p>
-        </section>
+        <><header className={styles["header-container"]}>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/students">Students</a></li>
+                    <li><a href="/teachers">Teachers</a></li>
+                    <li><a href="/classes">Classes</a></li>
+                </ul>
+            </nav>
+        </header>
+            <section className={styles["home-container"]}>
+                <h1>Home Page</h1>
+                <p>Welcome to the Home Page of the School Manager App!</p>
+                <p>Please, select a section from the navigation menu.</p>
+            </section>
+        </>
     );
 }
 export default Home;
