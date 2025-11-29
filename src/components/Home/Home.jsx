59868fc83a55ddf54dc04946242231463b023d39
jmoreno-dev <ytanonim12@gmail.com>
2025-11-29 12:07:42 +0100

    added people component to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,16 @@ import styles from "./Home.module.css";
 
 function Home() {
     return (
-        <><header className={styles["header-container"]}>
-            <nav>
-                <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/students">Students</a></li>
-                    <li><a href="/teachers">Teachers</a></li>
-                    <li><a href="/classes">Classes</a></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header className={styles["header-container"]}>
+                <nav>
+                    <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/people">People</a></li>
+                        <li><a href="/classes">Classes</a></li>
+                    </ul>
+                </nav>
+            </header>
             <section className={styles["home-container"]}>
                 <h1>Home Page</h1>
                 <p>Welcome to the Home Page of the School Manager App!</p>
