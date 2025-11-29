99eacaf0df6e01097ac86c5ba766fd60f07506de
jmoreno-dev <ytanonim12@gmail.com>
2025-11-29 13:05:53 +0100

    finished layout of students and teachers

--- src/components/Classes/Classes.jsx
+++ src/components/Classes/Classes.jsx
@@ -1,7 +1,10 @@
 import styles from './Classes.module.css';
+import NavBar from '../NavBar/NavBar.jsx';
+
 function Classes() {
     return (
         <>
+            <NavBar />
             <h1>Classes Component</h1>
         </>
     );
--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,8 +1,43 @@
 import styles from './Teachers.module.css';
+import NavBar from '../NavBar/NavBar.jsx';
+import { useState } from 'react';
 function Teachers() {
+    const [teachers] = useState([
+        { id: 1, name: 'Mr. Smith', class: 'Math' },
+        { id: 2, name: 'Ms. Johnson', class: 'Science' },
+        { id: 3, name: 'Mrs. Brown', class: 'History' },
+    ]);
     return (
         <>
             <NavBar />
+            <h1>Teachers</h1>
+            <section>
+                <table className={styles["teachers-table"]}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Class</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {teachers.map((teacher) => (
+                            <tr key={teacher.id}>
+                                <td>{teacher.id}</td>
+                                <td>{teacher.name}</td>
+                                <td>{teacher.age}</td>
+                                <td>{teacher.class}</td>
+                                <td>
+                                    <button>Update</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </section>
         </>
     );
 }
